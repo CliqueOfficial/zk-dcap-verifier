44e59f2095f3da9ace91d783086df81ce541d4be
Alan WANG <alan1995wang@outlook.com>
2024-02-06 22:10:17 +0800

    chore: make signature mod public

--- crates/dcap/src/lib.rs
+++ crates/dcap/src/lib.rs
@@ -2,10 +2,10 @@ mod cert;
 mod ecdsa_sig;
 mod enclave;
 mod quote;
-mod signature;
 mod tcb_info;
 mod traits;
 
+pub mod signature;
 pub use ecdsa_sig::*;
 pub use quote::*;
 pub use traits::{BinRepr, Verifiable};
