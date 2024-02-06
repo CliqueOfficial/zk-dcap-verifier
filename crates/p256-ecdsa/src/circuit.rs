fbfe74421f76bd305f2eca239583f635b83489ea
Alan WANG <alan1995wang@outlook.com>
2024-02-06 22:20:54 +0800

    chore: sort code

--- crates/p256-ecdsa/src/circuit.rs
+++ crates/p256-ecdsa/src/circuit.rs
@@ -27,32 +27,29 @@ pub fn ecdsa_verify(
     const NUM_LIMBS: usize = 3;
 
     let range = RangeChip::new(LOOKUP_BITS, builder.lookup_manager().clone());
+    let fp_chip = FpChip::new(&range, LIMB_BITS, NUM_LIMBS);
+    let fq_chip = FqChip::new(&range, LIMB_BITS, NUM_LIMBS);
+    let gate = GateChip::new();
+    let ecc_chip = EccChip::new(&fp_chip);
 
     let ctx = builder.main(0);
 
-    let res = {
-        let range = &range;
-        let fp_chip = FpChip::new(range, LIMB_BITS, NUM_LIMBS);
-        let fq_chip = FqChip::new(range, LIMB_BITS, NUM_LIMBS);
-
+    {
         let [m, r, s] = [input.msghash, input.r, input.s].map(|x| fq_chip.load_private(ctx, x));
 
         make_public.extend(m.limbs());
         make_public.extend(r.limbs());
         make_public.extend(s.limbs());
 
-        let ecc_chip = EccChip::new(&fp_chip);
         let pk = ecc_chip.load_private_unchecked(ctx, (input.x, input.y));
 
         make_public.extend(pk.x().limbs());
         make_public.extend(pk.y().limbs());
 
-        // test ECDSA
-        ecdsa_verify_no_pubkey_check::<_, Fp, Fq, Affine>(&ecc_chip, ctx, pk, r, s, m, 4, 4)
+        let res =
+            ecdsa_verify_no_pubkey_check::<_, Fp, Fq, Affine>(&ecc_chip, ctx, pk, r, s, m, 4, 4);
+        gate.assert_is_const(ctx, &res, &Fr::one());
     };
-
-    let gate = GateChip::new();
-    gate.assert_is_const(ctx, &res, &Fr::one());
 
     Ok(())
 }
